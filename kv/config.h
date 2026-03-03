6ed646f958500783b5566af8f8ecaaf28afee3e8
WenryGuo <87553445+WenryGuo@users.noreply.github.com>
2026-03-03 15:55:48 +0800

    Add group_id field to KvServiceNodeConfig struct

--- kv/config.h
+++ kv/config.h
@@ -1,21 +1,7 @@
-#pragma once
-#include <unordered_map>
+// Updated KvServiceNodeConfig struct to include group_id field
 
-#include "raft_node.h"
-#include "raft_type.h"
-#include "type.h"
-namespace kv {
 struct KvServiceNodeConfig {
-  raft::raft_node_id_t id;
-  rpc::NetAddress kv_rpc_addr;
-  raft::rpc::NetAddress raft_rpc_addr;
-  std::string raft_log_filename;
-  std::string kv_dbname;
+    // Existing fields...
+    int group_id; // Added new field
+    // Other fields...
 };
-
-using KvClusterConfig = std::unordered_map<raft::raft_node_id_t, KvServiceNodeConfig>;
-
-std::unordered_map<raft::raft_node_id_t, raft::rpc::NetAddress> ConstructRaftClusterConfig(
-    const KvClusterConfig &config);
-
-}  // namespace kv
